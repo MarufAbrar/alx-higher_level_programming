cc6b5134089a88b18f633e3deb91572ef80b3784
MarufAbrar <maracode1@gmail.com>
2022-06-28 16:41:30 +0300

    try 2

--- 0x00-python-hello_world/10-main.c
+++ 0x00-python-hello_world/10-main.c
@@ -1,53 +1,32 @@
+#!/usr/bin/python3
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
+ * check_cycle - Checks if a singly-linked list contains a cycle.
+ * @list: A singly-linked list.
  *
- * Return: Always 0.
+ * Return: If there is no cycle - 0.
+ *         If there is a cycle - 1.
  */
-int main(void)
+int check_cycle(listint_t *list)
 {
-  listint_t *head;
-  listint_t *current;
-  listint_t *temp;
-  int i;
+	listint_t *mbuzi, *kondoo;
 
-  head = NULL;
-  add_nodeint(&head, 0);
-  add_nodeint(&head, 1);
-  add_nodeint(&head, 2);
-  add_nodeint(&head, 3);
-  add_nodeint(&head, 4);
-  add_nodeint(&head, 98);
-  add_nodeint(&head, 402);
-  add_nodeint(&head, 1024);
-  print_listint(head);
+	if (list == NULL || list->next == NULL)
+		return (0);
 
-  if (check_cycle(head) == 0)
-    printf("Linked list has no cycle\n");
-  else if (check_cycle(head) == 1)
-    printf("Linked list has a cycle\n");
+	mbuzi = list->next;
+	kondoo = list->next->next;
 
-  current = head;
-  for (i = 0; i < 4; i++)
-    current = current->next;
-  temp = current->next;
-  current->next = head;
+	while (mbuzi && kondoo && kondoo->next)
+	{
+		if (mbuzi == kondoo)
+			return (1);
 
-  if (check_cycle(head) == 0)
-    printf("Linked list has no cycle\n");
-  else if (check_cycle(head) == 1)
-    printf("Linked list has a cycle\n");
+		mbuzi = mbuzi->next;
+		kondoo = kondoo->next->next;
+	}
 
-  current = head;
-  for (i = 0; i < 4; i++)
-    current = current->next;
-  current->next = temp;
-
-  free_listint(head);
-
-  return (0);
+	return (0);
 }
