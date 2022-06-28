5e32d77aba2c6e2616fb8e5619a821a653dfffaf
MarufAbrar <maracode1@gmail.com>
2022-06-28 16:21:12 +0300

    try now

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(str*3)
-print(str[:9])
+print(str + str + str)
+str2 = str[0]+str[1]+str[2]+str[3]+str[4]+str[5]+str[6]+str[7]+str[8]
+print(str2)
