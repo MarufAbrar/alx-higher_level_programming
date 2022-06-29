44b0ba9abc6c32911279e283f1e019246f9d06d9
MarufAbrar <maracode1@gmail.com>
2022-06-29 06:48:45 +0300

    trynowmaruf

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(str + str + str)
-str2 = str[0]+str[1]+str[2]+str[3]+str[4]+str[5]+str[6]+str[7]+str[8]
-print(str2)
+print(3*'{}'.format(str))
+print('{:.9}'.format(str))
