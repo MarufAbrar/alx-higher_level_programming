9bf73875d172e4006db2071ec59725a0fdd82faf
MarufAbrar <maracode1@gmail.com>
2022-07-27 07:35:43 +0300

    now

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,40 +1,23 @@
 #!/usr/bin/python3
 """
-File: 5-text_indentation.py
+This module defines `text_indentation`
 
-By: Maruf Abrar <maracode1@gmail.com>
-
-Defines function that prints a text with 
-2 new lines after each of these characters: ., ? and :
+The function prints a text with 2 new lines after each of
+these characters: ., ? and :
 """
 
 
 def text_indentation(text):
-    """
-    Function that prints a text with 2 new lines 
-    after each of these characters: ., ? and :
+    """adds paragraph after `.`, `:` and `?`
 
     Args:
-        text (str): text input
-    Raises:
-        TypeError: if text is not a string
-    There should be no space at the beginning 
-    or at the end of each printed line
+        text (str)
     """
     if not isinstance(text, str):
-        raise TypeError("text must be a string")
+        raise TypeError('text must be a string')
 
-    c = 0
-    while c < len(text) and text[c] == ' ':
-        c += 1
-
-    while c < len(text):
-        print(text[c], end="")
-        if text[c] == "\n" or text[c] in ".?:":
-            if text[c] in ".?:":
-                print("\n")
-            c += 1
-            while c < len(text) and text[c] == ' ':
-                c += 1
-            continue
-        c += 1
+    for l in '.:?':
+        text = text.replace(l, '{}\n'.format(l))
+    lines = text.splitlines()
+    for index, line in enumerate(lines):
+        print(line.strip(), end='' if index == len(lines) - 1 else '\n\n')
