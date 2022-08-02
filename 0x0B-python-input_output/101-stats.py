b6cf12759e809d1d864c8c947ffc85cd6d7f4d26
MarufAbrar <maracode1@gmail.com>
2022-08-02 15:41:38 +0300

    uu

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import 101-generator
 """Reads from standard input and computes metrics.
 After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
