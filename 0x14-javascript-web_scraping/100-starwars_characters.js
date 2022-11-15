664bf561586732af109f167572fb6fdb6ca1fd5a
MarufAbrar <maracode1@gmail.com>
2022-11-15 23:06:10 +0300

    now check alx

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const request = require('request');
+
+const episodeId = process.argv[2];
+
+if (parseInt(episodeId) < 8) {
+  const url = 'https://swapi-api.hbtn.io/api/films/' + episodeId;
+
+  request(url, (err, res, body) => {
+    if (err) {
+      return console.log(err);
+    }
+    const { characters } = JSON.parse(body);
+
+    characters.forEach(
+      (url, index) => {
+        request(url, (err, res, body) => {
+          if (err) {
+            return console.log(err);
+          }
+          console.log(JSON.parse(body).name);
+        });
+      });
+  });
+}
